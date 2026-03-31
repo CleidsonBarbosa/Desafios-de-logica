d9a78fa1bedf01347f44832648ee4a50a332e07a
Cleidson Barbosa <cleidson10293346@edu.pa.senac.br>
2026-03-31 20:58:43 -0300

    data de nascimento

--- 04_calculadora_de_idade.py
+++ 04_calculadora_de_idade.py
@@ -1,3 +1,8 @@
 '''Desenvolva um sistema que solicite o ano de nascimento do usuário, calcule e exiba a sua idade atual'''
-ano_nascimento = int(input('Digite o seu ano de nascimento:'))
-idade == ano_nascimento - data.today
+from datetime import date
+hoje = date.today()
+#print("A data de hoje é:", hoje)
+print(f"A data de hoje é:", hoje.strftime('%d/%m/%Y'))
+ano_nascimento = int(input("Digite sua data de nascimento: "))
+idade = hoje.year - ano_nascimento
+print(idade)
